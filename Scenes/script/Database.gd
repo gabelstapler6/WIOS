fd3f371143ad1eb47cf32e74ae06cdf4bf652389
Andi <andreas.braun98@outlook.de>
2020-09-21 13:24:02 +0200

    datenbankschnittstelle fertig

--- Scenes/script/Database.gd
+++ Scenes/script/Database.gd
@@ -7,11 +7,15 @@ var path_to_db = "res://data"
 
 var Player_Table = "Player"
 var where_user_is = "username = "
+var logged_player = {}
 
 var Items_Table = "Items"
+var items_array = []
 
 var Inventory_Table = "Inventory"
-var inventory_array
+# var inventory_array = []
+
+var current_select
 
 
 # Called when the node enters the scene tree for the first time.
@@ -27,59 +31,81 @@ func _ready():
 	
 	db.verbose_mode = true
 	db.foreign_keys = true
+	
+	current_select = db.select_rows(Items_Table, "", ["*"])
+	for i in current_select:
+		items_array.append(i)
+	
 	# db.import_from_json("res://json_export.json")
 	db.export_to_json("res://data.json")
-	# alle items in ein Array speichern
 	
+	# TODO: das entfernen (zum testen) #################################################
 	if check_password("yunggabel", "yunggabel123"):
 		setup_inventory()
+		retrieve_player_values()
 
 func check_password(username, pw):
 	username = "'" + username + "'"
-	var current_player = db.select_rows(Player_Table, where_user_is + username, ["*"])
+	current_select = db.select_rows(Player_Table, where_user_is + username, ["*"])
 	
-	for i in current_player:
+	for i in current_select:
 		if i["password"] == pw:
 			# sobald das hier ausgefuehrt wird ist der Spieler eingeloggt
 			where_user_is += username
+			logged_player = {
+				"ID": i["ID"],
+				"username": i["username"],
+				"scoreBalance": i["scoreBalance"],
+				"highscore": i["highscore"]
+				}
 			return true
 			
 	return false
 
 # fuellt das inventory des Players mit den Startitems, falls noch nicht getan
 func setup_inventory():	
-	db.select_rows(Inventory_Table)
+	current_select = db.select_rows(Inventory_Table, "playerID=" + str(logged_player.ID), ["*"])
+	if current_select.size() == 3:
+		return
 	
-	# eine 2. db verbindung muss eingerichtet werden um mehrere Tabellen in arrays zu speichern
-	var db2 = SQLite.new()
-	db2.path = path_to_db
-	db2.open_db()
-	var current_player = db2.select_rows(Player_Table, where_user_is, ["*"])
-	
-	var items_array = db.select_rows(Items_Table, "", ["*"])
+	current_select = db.select_rows(Items_Table, "", ["*"])
 	
 	var def_stock = 0
 	var join = []
-	for i in current_player:
-		for j in items_array:
-			if j["name"] == "AmmoIncrease":
-				def_stock = 1
-			join.append({"stock": def_stock, "playerID": i["ID"], "itemID": j["ID"]})
-			def_stock = 0
+	for j in current_select:
+		if j["name"] == "AmmoIncrease":
+			def_stock = 1
+		join.append({"stock": def_stock, "playerID": logged_player.ID, "itemID": j["ID"]})
+		def_stock = 0
 			
-	db2.close_db()
 	db.insert_rows(Inventory_Table, join)
 
 
 func update_player_scoreBalance(scoreBalance):
 	db.update_rows(Player_Table, where_user_is, {"scoreBalance": scoreBalance})
 	
+
 func update_player_highscore(run_score):
-	var selected_array = db.select_rows(Player_Table, where_user_is, ["highscore"])
-	
-	for i in selected_array:
+	current_select = db.select_rows(Player_Table, where_user_is, ["highscore"])
+	for i in current_select:
 		if i["highscore"] < run_score:
 			db.update_rows(Player_Table, where_user_is, {"highscore": run_score})
 
+
 func update_item_stock(item_name, stock):
-	pass
+	for i in items_array:
+		if i["name"] == item_name:
+			db.update_rows(Inventory_Table, "itemID=" + str(i["ID"]), {"stock": stock})
+			return
+
+func retrieve_player_values():
+	var dict = {
+		"username": logged_player.username,
+		"scoreBalance": logged_player.scoreBalance,
+		"highscore": logged_player.highscore
+	}
+	current_select = db.select_rows("Inventory, Items", "playerID=" + str(logged_player.ID) + " AND Items.ID = itemID", ["*"])
+	for i in current_select:
+		dict[i["name"] + "_stock"] = i["stock"]
+		
+	return dict
